b589388430ba6cf0a6dabf9d9dcb3a13430a9da9
johon <johonyolchiyev31@gmail.com>
2022-11-18 16:47:49 +0500

    first commit

---
+++ src/shared_components/Layout/footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="p-4 mt-12 text- w-full gap-9 bg-black text-white flex items-center justify-between">
+      01MAHKAMOV
+    </footer>
+  );
+}
+
+export default Footer;
+
